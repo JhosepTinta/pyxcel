676917da933267f2bccb60e1a80049186b7abedb
unknown <carpioandre7@gmail.com>
2021-11-12 15:10:13 -0400

    uniendo el codigo de las HU

---
+++ assets/js/script-registro.js
@@ -0,0 +1,147 @@
+import {app,db,ref,set,get,child,onChildAdded} from "../js/connection-firebase.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https:www.gstatic.com/firebasejs/9.1.2/firebase-auth.js"
+
+
+const auth = getAuth(app);
+   
+var inputs = document.querySelectorAll('.seccion-input input');
+
+const expresiones = {
+    nombre: /^\s*[a-zA-Z\s]{3,40}\s*$/,
+    correo: /^\s*[a-zA-Z0-9\-\_]+@[a-zA-Z0-9\-\_]+\.com+\s*$/,
+    contrasena: /^.{8,25}$/
+}
+
+
+agregarEventoBotonRegistrar();
+agregarEventsInputs();
+inciarBotonOjo();
+
+function insertarDatos(usuario) {
+    set(ref(db, "Usuarios/" + usuario.nombre), usuario)
+        .then(() => {
+            alert("Datos registrados correctamente");
+        })
+        .catch((error) => {
+            alert("unsucessfull, error" + error);
+        });
+}
+
+async function crearCuentaFirebase(usuario, contrasena) {
+    await createUserWithEmailAndPassword(auth, usuario.correo, contrasena).then((userCredential) => {
+        insertarDatos(usuario);
+        console.log("cuenta creada correctamente");
+      })
+      .catch((error) => {
+        console.log("no se pudo crear la cuenta");
+        motrarMensajeErrorInput('m-correo','Este correo ya esta registrado');
+        
+      });
+}
+
+async function agregarEventoBotonRegistrar(){
+      var boton = document.querySelector('#formulario');
+   
+      boton.addEventListener('submit',(e) =>{
+        var hayError = 0;
+          e.preventDefault();
+          inputs.forEach(element =>{
+            hayError += comprobarCampos(element);
+          });
+
+          if(hayError == 0){
+            var nombreR = document.querySelector(".input-nombre");
+            var correoR = document.querySelector(".input-correo");
+            var contrasenaR = document.querySelector(".input-contrasena");
+            var usuario= {
+                nombre: nombreR.value,
+                correo: correoR.value,
+            };
+            crearCuentaFirebase(usuario,contrasenaR.value);
+          }
+          console.log('registro enviado')
+          console.log(hayError);
+
+      });
+}
+
+
+
+function inciarBotonOjo(){
+    var boton = document.querySelector(".boton-ojo");
+    boton.addEventListener("click", ()=>{
+     
+        var inputContrasena = document.querySelector(".input-contrasena");
+        if(inputContrasena.type == "password"){
+            inputContrasena.type = "text";
+            boton.classList.add("mostrar-contrasena");
+            boton.classList.remove("ocultar-contrasena");
+
+
+        }else{
+            inputContrasena.type = "password";
+            boton.classList.remove("mostrar-contrasena");
+            boton.classList.add("ocultar-contrasena");
+
+        }
+    });
+}
+
+function agregarEventsInputs(){
+    inputs.forEach(element => {
+
+        element.addEventListener('keyup',(e)=>{
+            comprobarCampos(e.target);
+        } );
+        
+        element.addEventListener('blur',(e)=>{
+            comprobarCampos(e.target);
+        } );
+    });
+
+}
+
+function motrarMensajeErrorInput(nombreInput,mensaje){
+    var aux= document.querySelector('.'+nombreInput);
+    aux.textContent = mensaje;
+
+}
+
+function borrarMensajeErrorInput(nombreInput){
+    var aux= document.querySelector('.'+nombreInput);
+    aux.textContent = '';
+
+}
+
+
+function comprobarCampos(e){
+    switch(e.classList[0]){
+        case "input-nombre":
+          if(expresiones.nombre.test(e.value.trim())){
+              borrarMensajeErrorInput('m-nombre');
+              return 0;
+          }else{
+               motrarMensajeErrorInput('m-nombre','El nombre debe tener de 3 a 40 caracteres sin contener números o símbolos.');
+               return 1;
+          }
+        break;
+        case "input-correo":
+            if(expresiones.correo.test(e.value)){
+                borrarMensajeErrorInput('m-correo');
+                return 0;
+            }else{
+                motrarMensajeErrorInput('m-correo','El correo debe seguir el formato user@example.com');
+                return 1;
+            }
+        break;
+        case "input-contrasena":
+            if(expresiones.contrasena.test(e.value)){
+                borrarMensajeErrorInput('m-contrasena');
+                return 0;
+            }else{
+                motrarMensajeErrorInput('m-contrasena','La contraseña tiene que estar entre 8 y 25 caracteres');
+                return 1;
+            }
+        break;
+    } 
+}
