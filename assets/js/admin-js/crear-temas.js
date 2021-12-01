08a15a4988bb0701f28f5dbb2abbe59f49d77a00
chkaren66 <chkaren29@gmail.com>
2021-12-01 01:02:20 -0400

    correcciones de interfaz

--- assets/js/admin-js/crear-temas.js
+++ assets/js/admin-js/crear-temas.js
@@ -25,6 +25,7 @@ class MostrarUI{
             `;
         listatemas.appendChild(element);
     }
+
     addUltimo(){
         const listatemas = document.getElementById("lista-de-temas");
         const element = document.createElement("div");
@@ -44,6 +45,8 @@ function addUltimo(){
     listatemas.appendChild(element);
  
 }
+
+
 function addTema(tarjetatema){
     const listatemas = document.getElementById("lista-de-temas");
     const element = document.createElement("div");
@@ -54,8 +57,8 @@ function addTema(tarjetatema){
                    <div class="numerotema"><p>${tarjetatema.numerotema}</p></div>
                     <div class="titulotema"><p>${tarjetatema.titulotema}</p></div>
                    <div class="botones">
-                     <button class="btn" id="btn-editar">Editar </button>
-                     <button class="btn" id="btn-eliminar">Eliminar </button>
+                     <button class="btn" id="btn-editar">Editar</button>
+                     <button class="btn" id="btn-eliminar"><img class="delete-content" src="../../assets/img/icons/eliminar.png" width="20px"alt=""> </button>
                     </div>
                 </div>
             </div>
@@ -91,7 +94,83 @@ await recuperarTemas(1).then((datos)=>{
     // e.preventDefault();
     }
 })
-console.log("mira");
+// -------------------------------------------------------
+
+var enFoco= false;
+var esDescrip = false;
+var esfile = false;
+document.getElementById("nombre-nivel").addEventListener("focus",function(){
+    enFoco=true;
+    console.log(enFoco);
+    const x = document.getElementById("desplegar");
+    x.style.display = "block";
+});
+document.getElementById("nombre-nivel").addEventListener("blur",function(){
+    enFoco=false;
+    console.log(enFoco);
+    const x = document.getElementById("desplegar");
+    
+    // x.style.display = "none";
+    
+});
+// document.getElementById("descripcion").addEventListener("focus", function(){
+//     esDescrip=true;
+//     console.log("d" +esDescrip);
+// })
+// document.getElementById("descripcion").addEventListener("blur", function(){
+//     esDescrip=false;
+//     console.log("d"+esDescrip);
+// })
+// document.getElementById("files").addEventListener("focus",function(){
+//     esfile=true;
+//     console.log("f"+esfile);
+// })
+// document.getElementById("files").addEventListener("blur",function(){
+//     esfile=false;
+//     console.log("f"+esfile);
+// })
+function inciarFoco(){
+        enFoco=false;
+        console.log(enFoco);
+        const x = document.getElementById("desplegar");
+        x.style.display = "none";
+    
+}
+inciarFoco();
+
+     
+     function verOcultarFormulario(){
+        
+         const x = document.getElementById("desplegar");
+         console.log("karem"+x)
+         if(x.style.display=== "none"){
+             x.style.display= "block";
+     
+         }else{
+             x.style.display="none";
+         }
+         console.log("Entro a la funcion");
+             
+         }
+
+// --------------------------------------------------------
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+
+    // files is a FileList of File objects. List some properties.
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                  f.size, ' bytes, last modified: ',
+                  f.lastModifiedDate.toLocaleDateString(), '</li>');
+    }
+    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+  }
+
+  document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+//   ---------------------------------------------------
+
 document.getElementById('boton-alerta-prueba').addEventListener('click',function(e){
     Swal.fire({
         title: "¿Estas seguro que deseas eliminar este tema?",
@@ -116,18 +195,18 @@ document.getElementById('boton-alerta-prueba').addEventListener('click',function
 })
 // document.getElementById('nombretitulo').innerHTML=()
 // Evento que sucedera 
-document.getElementById('product-form').addEventListener('submit',function(e){
+// document.getElementById('product-form').addEventListener('submit',function(e){
     
-    const titulo = document.getElementById('tema').value;
-    const numerotema= document.getElementById('name').value;
-    const tarjetatema= new TarjetaTema(titulo,numerotema);
+//     const titulo = document.getElementById('tema').value;
+//     const numerotema= document.getElementById('name').value;
+//     const tarjetatema= new TarjetaTema(titulo,numerotema);
 
-    const contenedor = new MostrarUI();
+//     const contenedor = new MostrarUI();
 
-    contenedor.addTema(tarjetatema);
-    // contenedor.addUltimo()
-    e.preventDefault();
-});
+//     contenedor.addTema(tarjetatema);
+//     // contenedor.addUltimo()
+//     e.preventDefault();
+// });
 // let botonEliminar = document.getElementById("btn-eliminar");
 // // console.log("presionaste eliminar");
 // botonEliminar.addEventListener("click",function(){
