8f672ba9057836dcac1b6e9f1e3658f040aff0e1
chkaren66 <chkaren29@gmail.com>
2021-12-03 01:26:20 -0400

    Uniendo

--- assets/js/admin-js/crear-temas.js
+++ assets/js/admin-js/crear-temas.js
@@ -1,8 +1,8 @@
-import { getDatabase, set } from '../connection-firebase.js';
+import { getDatabase, set ,remove, ref} from '../connection-firebase.js';
 import { recuperarTemas, recuperarContenido, recuperarNivel} from '../recover-data.js';
 //Variables importantes
 // numero = numero del nivel que mostrar la interfaz
-const numero=1;
+const numero=2;
 
 // Ingresar datos de nuevo nivel a la base de datos
  const db = getDatabase();
@@ -27,14 +27,17 @@ function addUltimo(){
 function addTema(tarjetatema){
     const listatemas = document.getElementById("lista-de-temas");
     const element = document.createElement("div");
+    let cadena = tarjetatema.numerotema[tarjetatema.numerotema.length-1]+"a"+numero;
+    console.log(cadena);
+    console.log("holaaaaaaaaaaaaaaaaaaaaaaaaaa");
     element.innerHTML = `
-            <div class="div-tarjeta">
+            <div class="div-tarjeta" >
                 <div class="tarjeta">
-                  <form name="tema" value="${tarjetatema.numerotema}" method="GET">
-                   <div class="numerotema"><p>${tarjetatema.numerotema}</p></div>
-                    <div class="titulotema"><p>${tarjetatema.titulotema}</p></div>
-                   <div class="botones">
-                     <button class="btn" id="btn-editar">Editar</button>
+                  <form name="tema" action="./contenido-tema.htm" value="${tarjetatema.numerotema}" method="GET" >
+                     <div name="nivel" value="${cadena}" class="numerotema"><p>Tema:${tarjetatema.numerotema}</p></div>
+                     <div class="titulotema"><p>${tarjetatema.titulotema}</p></div>
+                     <div class="botones">
+                     <button name="tema" value="${cadena}" class="btn" id="btn-editar">Editar</button>
                      <div class="btn" id="btn-eliminar"><img class="delete-content" src="../../assets/img/icons/eliminar.png" width="20px"alt=""> </div>
                     </div>
                   </form>
@@ -56,8 +59,8 @@ await recuperarTemas(numero).then((datos)=>{
         console.log(objeto);
         for(const elemento in objeto){
             console.log(objeto[elemento].datos.titulo);
-            
-            const tarjetatema= new TarjetaTema( 'Tema: '+ contador  ,objeto[elemento].datos.titulo   );
+            console.log(elemento);
+            const tarjetatema= new TarjetaTema( elemento ,objeto[elemento].datos.titulo   );
             addTema(tarjetatema);
             console.log(contador);
             contador++;
@@ -75,7 +78,8 @@ await recuperarTemas(numero).then((datos)=>{
 function addEliminarBotones(element){
     element.addEventListener('click', (e)=>{
         console.log("holass......");
-        lanzarAlertaBorrar();
+        console.log(element.parentNode.parentNode.getAttribute("value"));
+        lanzarAlertaBorrar(element.parentNode.parentNode.getAttribute("value"));
         // e.preventDefault();
     })
 }
@@ -163,9 +167,23 @@ inciarFoco();
 //   document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
 //   ------ALERTA---------------------------------------------
+function borrarTemmas(nivelN,temaN){
+    remove(ref(db,"Temas/nivel"+ nivelN+"/"+temaN))
+    .then(() => {
+    //   alert("data removed bien")
+    })
+    .catch((error) => {
+      alert("error")
+    //   Swal.fire({
+    //     icon: 'error',
+    //     title: 'Oops...',
+    //     text: 'El tema no se ha eliminado correctamente!',
+        
+    //   })
+    })
+  }
 
-
-function lanzarAlertaBorrar(){
+function lanzarAlertaBorrar(temaN){
     Swal.fire({
         title: "¿Estas seguro que deseas eliminar este tema?",
         showCancelButton:true,
@@ -179,6 +197,7 @@ function lanzarAlertaBorrar(){
     }).then((result) => {
         if(result.isConfirmed){
             Swal.fire('Conformacion de eliminacion')
+            borrarTemmas(numero,temaN)
         }else if(result.dismiss){
             Swal.fire('cancelado')
         }
